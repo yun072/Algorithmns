fe4e724300d55877c64986b5741ccb70c27583da
yun072 <140836341+yun072@users.noreply.github.com>
2024-02-02 00:09:13 +0900

    [Bronze V] Title: 사칙연산, Time: 212 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in) ;
+		int A = scan.nextInt() ;
+		int B = scan.nextInt() ;
+		
+		System.out.println(A+B) ;
+		System.out.println(A-B) ;
+		System.out.println(A*B) ;
+		System.out.println(A/B) ;
+		System.out.println(A%B) ;
+		
+		
+
+	}
+
+}
