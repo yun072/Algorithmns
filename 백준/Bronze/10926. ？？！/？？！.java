eb7d71ed4a2a1f75f16054d8c4979f4679a2b013
yun072 <140836341+yun072@users.noreply.github.com>
2024-02-02 00:10:39 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in) ;
+		String Q = "??!" ;
+		
+		String I = scan.next() ;
+		System.out.println(I + Q);
+		
+		
+	
+		
+		
+
+	}
+
+}
