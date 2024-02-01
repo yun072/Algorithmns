61604d670aa8aaffff5f06f1ab474955364c8e26
yun072 <140836341+yun072@users.noreply.github.com>
2024-02-02 00:08:13 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in) ;
+		double A = scan.nextInt() ;
+		double B = scan.nextInt() ;
+		
+		System.out.println(A/B) ;
+		
+		
+
+	}
+
+}
