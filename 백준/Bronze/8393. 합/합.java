33dc2e9153d7393907c6f5ddf5da5e204c4ce65e
yun072 <140836341+yun072@users.noreply.github.com>
2024-02-04 15:09:11 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in) ;
+		
+		
+		int c = scan.nextInt();
+		int sum = 0;
+		for (int k = 1; k <= c; k++ ) {
+			sum += k ;
+			
+		}
+		System.out.println(sum) ;
+		
+			
+		
+		
+	
+			
+
+	}
+
+}
