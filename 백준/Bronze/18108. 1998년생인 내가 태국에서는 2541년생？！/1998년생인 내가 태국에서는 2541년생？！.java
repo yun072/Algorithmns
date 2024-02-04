8a7187a2d9fd492f5d53f0c4cd8898cea88542b6
yun072 <140836341+yun072@users.noreply.github.com>
2024-02-04 13:27:46 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in) ;
+		int A = scan.nextInt() ;
+		
+		
+		System.out.println( A - 543) ;
+		
+		
+
+	}
+
+}
