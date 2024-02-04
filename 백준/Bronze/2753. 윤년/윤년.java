a93a04466956c051177e74502b410d9bb1bbf741
yun072 <140836341+yun072@users.noreply.github.com>
2024-02-04 14:11:40 +0900

    [Bronze V] Title: 윤년, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in) ;
+		
+		int year = scan.nextInt();
+		
+		if (year%4 == 0 && year%100 != 0) {
+			System.out.println("1");
+		}else if (year % 400 == 0) {
+			System.out.println("1");
+		}else {
+			System.out.println("0");
+		}
+			
+
+	}
+
+}
