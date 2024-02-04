e99ffee5eb7e30eb3f81abd2ef2e18401f5f0902
yun072 <140836341+yun072@users.noreply.github.com>
2024-02-04 13:36:13 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+        
+		long a = scan.nextLong();
+        long b = scan.nextLong();
+        long c = scan.nextLong();
+
+        System.out.println(a+b+c);
+    }
+}
